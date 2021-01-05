22f7a62e78340433a1baf42bd2fd04b80a34bdbe
Alex Varjabedian <alex.var@simplepractice.com>
2021-01-05 13:32:35 -0800

    Managing Application-level Configurations - ENV variables

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoidGVzdGVtYmVyIiwiYSI6ImNramtocnVzNTAwZWoycW4wdXc2eGF6YWIifQ.ZFQJp4f8jnHTBHKef3CbZg'
+
   return ENV;
 };
