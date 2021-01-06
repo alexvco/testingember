3a0bef60caefe9c09735e9a213a457db8eebea1b
Alex Varjabedian <alex.var@simplepractice.com>
2021-01-06 14:09:09 -0800

    We have to set computed on src with the dependencies (lng, lat...) so that Ember knows when to "bust its cache" and re-render the component - this is how we auto-track without using glimmer (the 3.16 tutorial implicitly uses glimmer - All arguments that can be accessed from this.args (in other words, this.args.*) are implicitly marked as @tracked by the Glimmer component superclass which the tutorial implicitly inherits from)

--- frontend/app/components/map.js
+++ frontend/app/components/map.js
@@ -1,9 +1,12 @@
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 import ENV from 'frontend/config/environment';
 
 const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static'
 
 export default class MapComponent extends Component {
+  // src is a computed property and has (lng, lat, width, height, zoom) as dependencies
+  @computed('lng', 'lat', 'width', 'height', 'zoom')
   get src() {
     let { lng, lat, width, height, zoom } = this;
 
