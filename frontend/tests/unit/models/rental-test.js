9ef0337e4af36b06dbb4e80b94797acd3062947a
alexv7 <alexvarjabedian7@gmail.com>
2021-01-07 14:30:12 -0800

    somewhat of a useless test for me as i'm already providing the rental_type from the backend instead of computing it like the tutorial does

--- frontend/tests/unit/models/rental-test.js
+++ frontend/tests/unit/models/rental-test.js
@@ -5,9 +5,24 @@ module('Unit | Model | rental', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it has the right rental type', function(assert) {
     let store = this.owner.lookup('service:store');
-    let model = store.createRecord('rental', {});
-    assert.ok(model);
+    let rental = store.createRecord('rental', {
+      id: 1,
+      title: 'Grand Old Mansion',
+      owner: 'Veruca Salt',
+      city: 'San Francisco',
+      location: {
+        lat: 37.7749,
+        lng: -122.4194,
+      },
+      category: 'Estate',
+      rental_type: 'Standalone',
+      bedrooms: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
+    });
+
+    assert.equal(rental.rental_type, 'Standalone');
   });
 });
