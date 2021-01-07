59c8fcfd7ffa7dc3d7bb9f8891e1ef300ae8d35e
Alex Varjabedian <alex.var@simplepractice.com>
2021-01-06 17:47:38 -0800

    syntax change: use rentals instead of parsed

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -3,7 +3,8 @@ import Route from '@ember/routing/route';
 export default class IndexRoute extends Route {
   async model() {
     let response = await fetch('http://localhost:3001/rentals.json');
-    let parsed = await response.json();
-    return parsed;
+    let rentals = await response.json();
+
+    return rentals;
   }
 }
