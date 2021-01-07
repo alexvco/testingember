3f229fedaa51728870f81324509c21f2b8cb00ae
alexv7 <alexvarjabedian7@gmail.com>
2021-01-07 11:25:07 -0800

    make tests pass by using The Router Service - Ember exposes this internal state (navigation, urls) through the router service, which we can inject into our component

--- frontend/app/components/share-button.js
+++ frontend/app/components/share-button.js
@@ -1,10 +1,14 @@
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 const TWEET_INTENT = 'https://twitter.com/intent/tweet';
 
 export default class ShareButtonComponent extends Component {
+  @service router
   get currentURL() {
-    return window.location.href;
+    // this.router.currentURL is an ember thing that returns the path,
+    // the following command returns a url object (it takes 2 args: the path and the origin)
+    return new URL(this.router.currentURL, window.location.origin);
   }
 
   get shareURL() {
