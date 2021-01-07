4853ac097c0063058cb407ca84722f2676251ace
alexv7 <alexvarjabedian7@gmail.com>
2021-01-07 10:25:37 -0800

    use <ShareButton> component in <Rental::Detailed> component

--- frontend/app/components/share-button.js
+++ frontend/app/components/share-button.js
@@ -1,4 +1,29 @@
 import Component from '@ember/component';
 
-export default Component.extend({
-});
+const TWEET_INTENT = 'https://twitter.com/intent/tweet';
+
+export default class ShareButtonComponent extends Component {
+  get currentURL() {
+    return window.location.href;
+  }
+
+  get shareURL() {
+    let url = new URL(TWEET_INTENT);
+
+    url.searchParams.set('url', this.currentURL);
+
+    if (this.attrs.text) {
+      url.searchParams.set('text', this.attrs.text);
+    }
+
+    if (this.attrs.hashtags) {
+      url.searchParams.set('hashtags', this.attrs.hashtags);
+    }
+
+    if (this.attrs.via) {
+      url.searchParams.set('via', this.attrs.via);
+    }
+
+    return url;
+  }
+}
