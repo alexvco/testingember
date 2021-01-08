de5021c8bd6936a80aa188be4b91e1010f067674
alexv7 <alexvarjabedian7@gmail.com>
2021-01-07 16:03:55 -0800

    return data from backend using jsonapi standard as its the default for Ember Data's default data protocol and interchange format

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,11 +1,55 @@
 class RentalsController < ApplicationController
-	def index
-		rentals = Rental.all
-		render json: rentals, status: :ok
-	end
+  def index
+    rentals = Rental.all
+    rentals_data_in_jsonapi = []
+    rentals.each do |rental|
+      rentals_data_in_jsonapi << {
+        id: rental.id,
+        type: 'rental',
+        attributes: {
+          title: rental.title,
+          owner: rental.owner,
+          city: rental.city,
+          location: {
+            lat: rental.location['lat'],
+            lng: rental.location['lng']
+          },
+          category: rental.category,
+          rental_type: rental.rental_type,
+          bedrooms: rental.bedrooms,
+          image: rental.image,
+          description: rental.description,
+          created_at: rental.created_at,
+          updated_at: rental.updated_at
+        }
+      }
+    end
+    render json: { data: rentals_data_in_jsonapi }, status: :ok
+  end
 
-	def show
-		rental = Rental.find(params[:id])
-		render json: rental, status: :ok
-	end
+  def show
+    rental = Rental.find(params[:id])
+    render json: {
+      data: {
+        id: rental.id,
+        type: 'rental',
+        attributes: {
+          title: rental.title,
+          owner: rental.owner,
+          city: rental.city,
+          location: {
+            lat: rental.location['lat'],
+            lng: rental.location['lng']
+          },
+          category: rental.category,
+          rental_type: rental.rental_type,
+          bedrooms: rental.bedrooms,
+          image: rental.image,
+          description: rental.description,
+          created_at: rental.created_at,
+          updated_at: rental.updated_at
+        }
+      }
+    }, status: :ok
+  end
 end
