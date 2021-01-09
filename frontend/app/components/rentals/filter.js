cf38f002ecc9be0fb5e5326c74bff2c0e4d0db78
Alex Varjabedian <alex.var@simplepractice.com>
2021-01-08 17:32:40 -0800

    Filter Rentals functionality - If we take a look at our <Rentals::Filter> component template, we see that we don't actually render any content. Instead, this component's only responsibility is to set up some piece of state (this.results, the list of filtered rental properties), and then yield that state back up to its caller (<Rentals>) in the form of a block parameter (as |filtered|). This is called the provider component pattern, which we see in action with one component providing data up to its caller.

--- frontend/app/components/rentals/filter.js
+++ frontend/app/components/rentals/filter.js
@@ -1,4 +1,15 @@
+import { computed } from '@ember/object';
 import Component from '@ember/component';
 
-export default Component.extend({
-});
+export default class RentalsFilterComponent extends Component {
+  @computed('rentals', 'query')
+  get results() {
+    let { rentals, query } = this;
+
+    if (query) {
+      rentals = rentals.filter(rental => rental.title.includes(query));
+    }
+
+    return rentals;
+  }
+}
